33460d8653ed2c5f310829bb8c9b1205efb1c146
Init Built <lauwrentius.m@vmgstudio520.com>
2018-05-31 13:57:16 -0700

    serrect key session not working.

--- application.py
+++ application.py
@@ -640,5 +640,6 @@ def addAWSHost():
 
 if __name__ == '__main__':
     application.secret_key = '1GrSamWXZ8ikGhg43UIUbw5X'
+    application.config['SESSION_TYPE'] = 'filesystem'
     application.debug = True
     application.run(host='0.0.0.0', port=5000)
